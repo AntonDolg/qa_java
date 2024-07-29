0151ed4c1fb8460a151a9c7e68424f10cf93f76c
AntonDolg <modbus7@gmail.com>
2024-07-29 17:55:00 +0300

    test2

--- box/src/main/java/ProgFirst.java
+++ box/src/main/java/ProgFirst.java
@@ -1,7 +1,6 @@
-package ru;
-
 public class ProgFirst {
     public static void main(String[] args) {
        System.out.println("Hi man");
+       System.out.println("what is it?"+ "chokopai");
    }
 }
