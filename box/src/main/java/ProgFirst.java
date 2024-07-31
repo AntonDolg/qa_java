f93a51b9bde4ea486ac6680269a25bcc4bd780e0
AntonDolg <modbus7@gmail.com>
2024-07-31 19:00:27 +0300

    31.07.24

--- box/src/main/java/ProgFirst.java
+++
@@ -1,6 +0,0 @@
-public class ProgFirst {
-    public static void main(String[] args) {
-       System.out.println("Hi man");
-       System.out.println("what is it?"+ "chokopai");
-   }
-}
---
+++ box/src/main/java/test/ProgFirst.java
@@ -0,0 +1,13 @@
+package test;
+
+public class ProgFirst {
+    public static void main(String[] args) {
+        String text = "british stylish";
+        voice(text);
+        //System.out.println("what is it?"+ "chokopai");
+   }
+
+    public static void voice(String newtext){
+        System.out.println(newtext);
+    }
+}
