9ddbca8fcbaf3ee0aec6e47164cbafa2acd0ba0a
AntonDolg <modbus7@gmail.com>
2024-07-31 20:09:44 +0300

    +square

--- box/src/main/java/math/ProgFirst.java
+++ box/src/main/java/math/ProgFirst.java
@@ -1,7 +1,11 @@
-package test;
+package math;
 
 public class ProgFirst {
     public static void main(String[] args) {
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной" + s.l + " = " + s.area());
+
+
         String text = "british stylish";
         voice(text);
         //System.out.println("what is it?"+ "chokopai");
